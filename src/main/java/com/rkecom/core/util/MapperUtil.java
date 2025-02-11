67e7fe176fcd41596c3373aabeb587075c59a3af
Radha Krishna <rkanugonda969@gmail.com>
2025-02-11 22:57:45 +0530

    enhanced MapperUtil

--- src/main/java/com/rkecom/core/util/MapperUtil.java
+++ src/main/java/com/rkecom/core/util/MapperUtil.java
@@ -1,5 +1,6 @@
 package com.rkecom.core.util;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.function.Consumer;
 
@@ -8,8 +9,24 @@ public class MapperUtil {
     }
 
     public static < T > void updateField( T currentValue, T newValue, Consumer<T> setter ) {
-        if(Objects.nonNull ( newValue ) && !newValue.equals ( currentValue ) ) {
+        if(Objects.nonNull ( newValue ) && !areEqual ( currentValue, newValue ) ) {
             setter.accept ( newValue );
         }
+    }
+
+    private static <T> boolean areEqual(T currentValue, T newValue) {
+        if (currentValue == newValue) {
+            return true; // Same reference, no update needed
+        }
+        if (currentValue == null || newValue == null) {
+            return false; // One is null, meaning they are not equal
+        }
+        if (currentValue instanceof BigDecimal c && newValue instanceof BigDecimal n) {
+            return c.compareTo(n) == 0;
+        }
+        if (currentValue instanceof Number c && newValue instanceof Number n) {
+            return c.doubleValue() == n.doubleValue();
+        }
+        return Objects.equals(currentValue, newValue);
     }
 }
